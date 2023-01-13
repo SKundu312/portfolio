c84d1a1af4a4a1ebb128f94314ebaf3450318841
Shreya <kundushreya312@gmail.com>
2023-01-13 17:16:42 +0530

    changes with logo

--- README.md
+++ README.md
@@ -52,7 +52,6 @@ Clone down this repository. You will need `node.js` and `git` installed globally
 ## 🛠 Installation and Setup Instructions
 
 1. Installation: `npm install`
-
 2. In the project directory, you can run: `npm start`
 
 Runs the app in the development mode.\
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Shreya Kundu</span>
+            Hi Everyone, I am <span className="purple">Shreya Kundu </span>
             from <span className="purple"> Lucknow, India.</span>
             <br />I am a pre-final year student pursuing B.Tech in Electronics and Communication Engineering from National Institute of Technology, Kurukshetra. 
             <br />
